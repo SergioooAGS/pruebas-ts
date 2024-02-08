44ff9906e64b8400f1f22adfe12eb005ee340f5c
SergioooAGS <serginchis@gmail.com>
2024-02-08 17:55:20 -0600

    Ajustes ts

--- src/app.ts
+++ src/app.ts
@@ -1,122 +1,102 @@
-// Me falta aun solo pasar unas funciones de html a type 
-class MiClase {
-  constructor() {
-    console.log('Se ha creado un constructor');
+document.addEventListener('DOMContentLoaded', function() {  // load el html pa manipular el dom
+    const body = d3.select('body');
+
+    const CSVG = body.append('svg')   //bote trash-id
+        .attr('id', 'miSVG2')
+        .attr('width', '1200')
+        .attr('height', '1220');
+    CSVG.append("image")  
+        .attr('href', 'images/trash.svg')
+        .attr('width', '200') 
+        .attr('height', '200'); 
+
+    const figurascontainer = CSVG.append("g");
+    //para arraastre del circulo
+    let offsetX = 0;
+    let offsetY = 0;
+
+   //crear figuras
+    function createCircle(cx: number, cy: number) {
+        const newCircle = figurascontainer.append("circle")
+            .attr("cx", cx)
+            .attr("cy", cy)
+            .attr("r", 50)
+            .attr("fill", "green")
+            .attr("cursor", "grab");
+        newCircle.call(d3.drag()
+            .on("start", dragStart)
+            .on("drag", dragging)
+            .on("end", dragEnd)
+        );
+    }
+
+    function createTriangle(x: number, y: number) {
+      const newTriangle = figurascontainer.append('path')
+          .attr('d', 'M ' + x + ' ' + y + ' L ' + (x + 50) + ' ' + (y + 50) + ' L ' + (x - 50) + ' ' + (y + 50) + ' Z')
+          .attr('fill', 'Red')
+          .attr("cursor", "grab");
+      newTriangle.call(d3.drag()
+          .on("start", dragStart)
+          .on("drag", dragging)
+          .on("end", dragEnd)
+      );
+    }
+
+    
+    function dragStart(event: any) {
+      const selection = d3.select(this);
+      offsetX = event.x - +selection.attr("cx") || 0;
+      offsetY = event.y - +selection.attr("cy") || 0;
   }
 
-  metodo() {
-    console.log('Imprime metodo');
+  function dragging(event: any) {
+    const selection = d3.select(this);
+    const newX = Math.max(50, Math.min(event.x - offsetX, 1150));
+    const newY = Math.max(50, Math.min(event.y - offsetY, 1170));
+    selection.attr("cx", newX).attr("cy", newY);
   }
-}
 
-// Crear una instancia de la clase y llamar a un método
-const instancia = new MiClase();
-instancia.metodo();
+  function dragEnd(event: any) {
+      const selection = d3.select(this);
+      const funX = +selection.attr("cx") || 0;
+      const funY = +selection.attr("cy") || 0;
+      const image = d3.select("image");
+      const imageX = +image.attr("x") || 0;
+      const imageY = +image.attr("y") || 0;
+      const imageWidth = +image.attr("width") || 0;
+      const imageHeight = +image.attr("height") || 0;
 
-
-const svgstar = d3.select('star');
-const body2 = d3.select('star');
-const button2 = body2.append('button2').text('Add Star')
-    //Esta en proceso esta parte
-
-const svg = d3.select("#miSVG2");
-//crear el circulo con d3
-const body = d3.select('body');
-const button = body.append('button').text('Add circle')
-  .style('position', 'fixed') 
-  .style('top', '20px') 
-  .style('left', '20px'); 
-button.on('click', () => {
-  createCircle(400, 400);
-  console.log("si se añadio");
-})
-
-
-svg.append("image")  
-.attr('href', 'images/trash.svg') //Es la ruta del archivo local que se manda a llamar
-.attr('width', '350') 
-.attr('height', '350'); 
-
-const circleContainer = svg.append("g");
-// Variables para que se mueva (arraastre)
-let offsetX = 0;
-let offsetY = 0;
-
-//  crear un nuevo círculo
-function createCircle(cx: number, cy: number) {
-  const newCircle = circleContainer.append("circle")
-    .attr("cx", cx)
-    .attr("cy", cy)
-    .attr("r", 50)
-    .attr("fill", "green") //color del circulo
-    .attr("cursor", "grab");
-
-  newCircle.call(d3.drag()
-    .on("start", dragStart)
-    .on("drag", dragging)
-    .on("end", dragEnd)
-  );
-}
-
-
-function handleClick() {
-  // Aplicar la transición al círculo
-  d3.select(this)
-    .transition()
-    .duration(1000)
-    .attr("r", 20)
-    .attr("fill", "yellow");
-}
-
-
-//Agregar eventos de mouse para el arrastre del círculo
-function dragStart(event: any) {
-
-  offsetX = event.x - +d3.select(this).attr("cx");
-  offsetY = event.y - +d3.select(this).attr("cy");
-}
-
-function dragging(event: any) {
-
-  const newX = event.x - offsetX;
-  const newY = event.y - offsetY;
-
-
-  d3.select(this)
-    .attr("cx", newX)
-    .attr("cy", newY);
-}
-
-function dragEnd(event: any) {
-  const circle = d3.select(this);
-  const circleX = +circle.attr("cx");
-  const circleY = +circle.attr("cy");
-
-  const image = d3.select("image");
-  const imageX = +image.attr("x");
-  const imageY = +image.attr("y");
-  const imageWidth = +image.attr("width");
-  const imageHeight = +image.attr("height");
-
-
-  if (
-    circleX >= imageX &&
-    circleX <= imageX + imageWidth &&
-    circleY >= imageY &&
-    circleY <= imageY + imageHeight
-  ) {
-    // pa aplicarlo se debe considerar sus propiedades para quele den estilo
-    circle.transition()
-      .duration(500)
-      .attr("r", 0)
-      .attr("fill", "transparent")
-      setTimeout(() => {
-      circle.remove();
-    }, 500);
+      if (
+          funX >= imageX &&
+          funX <= imageX + imageWidth &&
+          funY >= imageY &&
+          funY <= imageY + imageHeight
+      ) {
+          selection.transition()
+              .duration(500)
+              .attr("r", 0)
+              .attr("fill", "transparent");
+          setTimeout(() => {
+              selection.remove();
+          }, 500);
+      }
   }
-}
 
+//botones generar figs
+    const buttonCirculo = body.append('button').text('Add circle')
+        .style('position', 'fixed') 
+        .style('top', '20px') 
+        .style('left', '20px'); 
+    buttonCirculo.on('click', () => {
+        createCircle(500, 500);
+    });
 
-
-
-
+    const buttonTriangle = body.append('button').text('Add triangle')
+        .style('position', 'fixed')
+        .style('top', '20px')
+        .style('left', '120px');
+    buttonTriangle.on('click', () =>{
+         createTriangle(500, 500);
+    });
+  }
+);
